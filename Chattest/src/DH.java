e44cf800a5ad24e811083709443ecec42061da6b
NicholasDollick <nidollick@gmail.com>
2018-08-25 23:33:41 -0700

    the mess gets bigger

--- Chattest/src/DH.java
+++ Chattest/src/DH.java
@@ -22,17 +22,54 @@ import javax.crypto.spec.DHPublicKeySpec;
  */
 
 public class DH {
-	private static final String TAG = "DH";
-	private static KeyPair keyPair;
-	private static PublicKey publicKey;
-	private static PrivateKey privateKey;
+	private final String TAG = "DH";
+	private KeyPair keyPair;
+	private PublicKey publicKey;
+	private PublicKey recievedPublicKey;
+	private PrivateKey privateKey;
 	private byte[] secretKey;
+	private byte[] pubKeyEnc;
 	
-	//public final static int pValue = 41;
-	//public final static int gValue = 83;
+	private static final byte P_BYTES[] = {
+    		(byte)0xF4, (byte)0x88, (byte)0xFD, (byte)0x58,
+            (byte)0x4E, (byte)0x49, (byte)0xDB, (byte)0xCD,
+            (byte)0x20, (byte)0xB4, (byte)0x9D, (byte)0xE4,
+            (byte)0x91, (byte)0x07, (byte)0x36, (byte)0x6B,
+            (byte)0x33, (byte)0x6C, (byte)0x38, (byte)0x0D,
+            (byte)0x45, (byte)0x1D, (byte)0x0F, (byte)0x7C,
+            (byte)0x88, (byte)0xB3, (byte)0x1C, (byte)0x7C,
+            (byte)0x5B, (byte)0x2D, (byte)0x8E, (byte)0xF6,
+            (byte)0xF3, (byte)0xC9, (byte)0x23, (byte)0xC0,
+            (byte)0x43, (byte)0xF0, (byte)0xA5, (byte)0x5B,
+            (byte)0x18, (byte)0x8D, (byte)0x8E, (byte)0xBB,
+            (byte)0x55, (byte)0x8C, (byte)0xB8, (byte)0x5D,
+            (byte)0x38, (byte)0xD3, (byte)0x34, (byte)0xFD,
+            (byte)0x7C, (byte)0x17, (byte)0x57, (byte)0x43,
+            (byte)0xA3, (byte)0x1D, (byte)0x18, (byte)0x6C,
+            (byte)0xDE, (byte)0x33, (byte)0x21, (byte)0x2C,
+            (byte)0xB5, (byte)0x2A, (byte)0xFF, (byte)0x3C,
+            (byte)0xE1, (byte)0xB1, (byte)0x29, (byte)0x40,
+            (byte)0x18, (byte)0x11, (byte)0x8D, (byte)0x7C,
+            (byte)0x84, (byte)0xA7, (byte)0x0A, (byte)0x72,
+            (byte)0xD6, (byte)0x86, (byte)0xC4, (byte)0x03,
+            (byte)0x19, (byte)0xC8, (byte)0x07, (byte)0x29,
+            (byte)0x7A, (byte)0xCA, (byte)0x95, (byte)0x0C,
+            (byte)0xD9, (byte)0x96, (byte)0x9F, (byte)0xAB,
+            (byte)0xD0, (byte)0x0A, (byte)0x50, (byte)0x9B,
+            (byte)0x02, (byte)0x46, (byte)0xD3, (byte)0x08,
+            (byte)0x3D, (byte)0x66, (byte)0xA4, (byte)0x5D,
+            (byte)0x41, (byte)0x9F, (byte)0x9C, (byte)0x7C,
+            (byte)0xBD, (byte)0x89, (byte)0x4B, (byte)0x22,
+            (byte)0x19, (byte)0x26, (byte)0xBA, (byte)0xAB,
+            (byte)0xA2, (byte)0x5E, (byte)0xC3, (byte)0x55,
+            (byte)0xE9, (byte)0x2F, (byte)0x78, (byte)0xC7
+    };
 	
-	private static final BigInteger P = BigInteger.probablePrime(512, new SecureRandom());
-	private static final BigInteger G = BigInteger.probablePrime(512, new SecureRandom());
+	//public final static int P = 41;
+	//public final static int G = 83;
+	
+	private static final BigInteger P = new BigInteger(1, P_BYTES);
+	private static final BigInteger G = BigInteger.valueOf(2);
 	
 	public byte[] generatePublicKey() throws NoSuchAlgorithmException, InvalidAlgorithmParameterException, InvalidKeyException {
 
@@ -64,10 +101,10 @@ public class DH {
 		return pubKey;
 	}
 	
-	public void generatePublicKey2() throws NoSuchAlgorithmException, InvalidAlgorithmParameterException, InvalidKeyException {
+	public void generateKeys() throws NoSuchAlgorithmException, InvalidAlgorithmParameterException, InvalidKeyException {
 
 		
-		System.out.println("[+] Generating Key...");
+		System.out.println("[+] Generating Keys...");
 		
 		DHParameterSpec dhParamSpec = new DHParameterSpec(P, G);
 		
@@ -79,23 +116,51 @@ public class DH {
 		
 		publicKey = keyPair.getPublic();
 		privateKey = keyPair.getPrivate();
+		pubKeyEnc = keyPair.getPublic().getEncoded();
 		System.out.println("Public Key = " + keyPair.getPublic());
+		System.out.println("Public Key Enc = " + keyPair.getPublic().getEncoded());
 		System.out.println("Private Key = " + keyPair.getPrivate());
 	}
 	
-	public void getShared(PublicKey recievedKey) throws NoSuchAlgorithmException, InvalidKeyException {
+	public void getShared() throws NoSuchAlgorithmException, InvalidKeyException {
 		final KeyAgreement keyAgree = KeyAgreement.getInstance(TAG);
 		keyAgree.init(privateKey);
-		keyAgree.doPhase(recievedKey, true);
+		keyAgree.doPhase(recievedPublicKey, true);
 		
 		secretKey = keyAgree.generateSecret();
 	}
 	
+	public void receiveKey(byte[] rKey) throws NoSuchAlgorithmException, InvalidKeySpecException, InvalidAlgorithmParameterException, InvalidKeyException {
+		KeyFactory keyFac = KeyFactory.getInstance("DH");
+		X509EncodedKeySpec x509KeySpec = new X509EncodedKeySpec(rKey);
+		PublicKey testKey = keyFac.generatePublic(x509KeySpec);
+		DHParameterSpec dhFromRKey = ((DHPublicKey)testKey).getParams();
+		KeyPairGenerator pairGen = KeyPairGenerator.getInstance(TAG);
+		pairGen.initialize(dhFromRKey);
+		KeyPair newPair = pairGen.generateKeyPair(); //maybe add to obj vars
+		KeyAgreement keyAgree = KeyAgreement.getInstance(TAG);
+		keyAgree.init(newPair.getPrivate());
+		byte[] newKeyEnc = newPair.getPublic().getEncoded();
+	}
+	
+    public void receivePublicKeyFrom(final DH person) {
+
+    	recievedPublicKey = person.getPublicKey();
+    }
+	
+    public PublicKey getPublicKey() {
+
+        return publicKey;
+    }
+    
+    public byte[] getPublicKeyEnc() {
+
+        return pubKeyEnc;
+    }
+	
 	public void printKeys() {
-		System.out.println("[+] Listing Keys");
-		System.out.println("Public Key = " + publicKey);
-		System.out.println("Private Key = " + privateKey);
-		System.out.println("Shared Key = " + secretKey);
+		System.out.println("[+] Listing Key");
+		System.out.println("Shared Key = " + this.secretKey);
 	}
 	
 	public byte[] computeSharedKey(byte[] pubKey) {
@@ -171,13 +236,7 @@ public class DH {
         }
         return buf.toString();
     }
-	
-	
-	//modify below
-    private static final byte P_BYTES[] = {
-            (byte)0xC4, (byte)0x88, (byte)0xFD, (byte)0x58,
-            (byte)0xE9, (byte)0x2F, (byte)0x78, (byte)0xC9
-    };
+    
     
     // main used for testing implementation
     public static void main(String[] args) throws NoSuchAlgorithmException, InvalidAlgorithmParameterException, InvalidKeyException, InvalidKeySpecException {
@@ -187,13 +246,29 @@ public class DH {
     	DH bob = new DH();
     	DH alice = new DH();
     	/*
-    	 * Alice creates key
+    	 * create keys
     	 */
+    	alice.generateKeys();
+    	bob.generateKeys();
     	//byte[] alicePubKey = alice.generatePublicKey();
     	
     	/*
-    	 * bob recieves key
+    	 * swap keys
     	 */
+    	alice.receivePublicKeyFrom(bob);
+    	bob.receivePublicKeyFrom(alice);
+    	
+    	/*
+    	 * generate shared
+    	 */
+    	alice.getShared();
+    	bob.getShared();
+    	
+    	bob.printKeys();
+    	alice.printKeys();
+    	
+    	bob.receiveKey(alice.getPublicKeyEnc());
+    	
     	//bob.generatePublicKey2();
     	//alice.generatePublicKey2();
     	
