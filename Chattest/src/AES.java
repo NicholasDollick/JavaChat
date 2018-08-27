b07ab3e74116b5092fbe50cd502bd424a13164cf
NicholasDollick <nidollick@gmail.com>
2018-08-26 17:02:56 -0700

    proper IV generation added to AES class

--- Chattest/src/AES.java
+++ Chattest/src/AES.java
@@ -23,6 +23,8 @@ import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.PBEKeySpec;
 import javax.crypto.spec.SecretKeySpec;
 
+import org.apache.commons.lang3.ArrayUtils;
+
 public class AES {
 	
 	private static final String ALGO = "AES";
@@ -36,23 +38,34 @@ public class AES {
 	public static byte[] encryptTest(final String data, byte[] secretKey) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException, InvalidAlgorithmParameterException {
 		final SecretKeySpec keySpec = new SecretKeySpec(secretKey, 0, 16, ALGO);
 		final Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5PADDING");
-		IvParameterSpec ivParameterSpec = new IvParameterSpec(secretKey);
+		SecureRandom rand = new SecureRandom();
+		byte[] iv = new byte[cipher.getBlockSize()];
+		rand.nextBytes(iv);
+		IvParameterSpec ivParameterSpec = new IvParameterSpec(iv);
 		
 		cipher.init(Cipher.ENCRYPT_MODE, keySpec, ivParameterSpec);
 		
 		final byte[] encryptedData = cipher.doFinal(data.getBytes());
+		System.out.println("\nTEST"); 
+		System.out.println(iv + " length: " + iv.length);
+		System.out.println(encryptedData + " length: " + encryptedData.length);
+		final byte[] concat = ArrayUtils.addAll(iv, encryptedData);
+		System.out.println(concat + " length: " + concat.length);
 		
-		return encryptedData;
+		return concat;
 	}
 	
 	public static String decryptTest(final byte[] data, byte[] secretKey) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException, InvalidAlgorithmParameterException {
 		final SecretKeySpec keySpec = new SecretKeySpec(secretKey, ALGO);
 		final Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5PADDING");
-		IvParameterSpec ivParameterSpec = new IvParameterSpec(secretKey);
+		byte[] iv = Arrays.copyOfRange(data, 0, 16);
+		byte[] message = Arrays.copyOfRange(data, 16, data.length);
+	
+		IvParameterSpec ivParameterSpec = new IvParameterSpec(iv);
 		
 		cipher.init(Cipher.DECRYPT_MODE, keySpec, ivParameterSpec);
 		
-		return new String(cipher.doFinal(data));
+		return new String(cipher.doFinal(message));
 	}
 	
 
