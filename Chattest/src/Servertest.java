175a674356b7913664967f074c950255e7670555
NicholasDollick <nidollick@gmail.com>
2018-09-06 18:28:49 -0700

    fixed issues and updated server UI

--- Chattest/src/Servertest.java
+++ Chattest/src/Servertest.java
@@ -5,10 +5,10 @@ public class Servertest
 
 	public static void main(String[] args) 
 	{
-		SimpleServer test = new SimpleServer();
+		//SimpleServer test = new SimpleServer();
 		
-		test.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		test.startRunning();
+		//test.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		//test.startRunning();
 
 	}
 
